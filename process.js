7026218d696ce42f0c84f68ab5f5852e92ea3e14
typescript-eslinter <knowhowdev@outlook.com>
2024-12-01 16:21:09 -0500

    update

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec(`pm2 start ${__dirname}/index.js --name eslinter --instances 1`, (error, stdout, stderr) => {
+exec(`pm2 start ${__dirname}/index.js --name eslinter --instances 1 --node-args="--max-old-space-size=4096"`, (error, stdout, stderr) => {
   if (error) {
     return;
   }
